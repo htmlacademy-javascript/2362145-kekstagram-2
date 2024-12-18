2b07320c4a187861ae61a20abce5f9fbf9bc1ead
Nikita7515 <152637405+Nikita7515@users.noreply.github.com>
2024-12-18 08:58:03 +0300

    немного доработки

--- js/function.js
+++ js/function.js
@@ -4,11 +4,17 @@ const chekLengthString = (chekString, maxLength) => {
   return cheking;
 };
 
+// Строка короче 20 символов
+chekLengthString('проверяемая строка', 20); // true
+// Длина строки ровно 18 символов
+chekLengthString('проверяемая строка', 18); // true
+// Строка длиннее 10 символов
+chekLengthString('проверяемая строка', 10); // false
+
+
 // Функция проверки на полиндром
 const isPolindrom = (string) => {
-  const formatSpaceString = (string.replaceAll(' ', ''));
-  const formatedString = (formatSpaceString.toLowerCase());
-
+  const formatedString = (string.replaceAll(' ', '').toLowerCase());
   let compareString = '';
 
   for (let i = formatedString.length - 1; i >= 0; i--) {
@@ -18,3 +24,12 @@ const isPolindrom = (string) => {
   const cheking = formatedString === compareString;
   return cheking;
 };
+
+// Строка является палиндромом
+isPolindrom('топот'); // true
+// Несмотря на разный регистр, тоже палиндром
+isPolindrom('ДовОд'); // true
+// Это не палиндром
+isPolindrom('Кекс'); // false
+// Это палиндром
+isPolindrom('Лёша на полке клопа нашёл '); // true
