0b2f605fd9c6b5147897dd383c164edad746bc89
Nikita7515 <152637405+Nikita7515@users.noreply.github.com>
2024-12-20 09:17:12 +0300

    Доработки

--- js/function.js
+++ js/function.js
@@ -1,15 +1,18 @@
 // Функция проверки длины строки
-const chekLengthString = (chekString, maxLength) => {
-  const cheking = chekString.length <= maxLength;
-  return cheking;
-};
+const chekLengthString = (chekString, maxLength) => chekString.length <= maxLength;
 
-// Строка короче 20 символов
-chekLengthString('проверяемая строка', 20); // true
-// Длина строки ровно 18 символов
-chekLengthString('проверяемая строка', 18); // true
-// Строка длиннее 10 символов
-chekLengthString('проверяемая строка', 10); // false
+// // Строка короче 20 символов
+// console.log('Тест №1: Должно быть true, выводит - ' +
+//   chekLengthString('проверяемая строка', 20) // true
+// );
+// // Длина строки ровно 18 символов
+// console.log('Тест №2 Должно быть true, выводит - ' +
+//   chekLengthString('проверяемая строка', 18) // true
+// );
+// // Строка длиннее 10 символов
+// console.log('Тест №3 Должно быть false, выводит - ' +
+//   chekLengthString('проверяемая строка', 10) // false
+// );
 
 
 // Функция проверки на полиндром
@@ -21,15 +24,22 @@ const isPolindrom = (string) => {
     compareString += formatedString.at([i]);
   }
 
-  const cheking = formatedString === compareString;
-  return cheking;
+  return formatedString === compareString;
 };
 
-// Строка является палиндромом
-isPolindrom('топот'); // true
-// Несмотря на разный регистр, тоже палиндром
-isPolindrom('ДовОд'); // true
-// Это не палиндром
-isPolindrom('Кекс'); // false
-// Это палиндром
-isPolindrom('Лёша на полке клопа нашёл '); // true
+// // Строка является палиндромом
+// console.log('Тест №4 Должно быть true, выводит - ' +
+//   isPolindrom('топот') // true
+// );
+// // Несмотря на разный регистр, тоже палиндром
+// console.log('Тест №5 Должно быть true, выводит - ' +
+//   isPolindrom('ДовОд') // true
+// );
+// // Это не палиндром
+// console.log('Тест №6 Должно быть false, выводит - ' +
+//   isPolindrom('Кекс') // false
+// );
+// // Это палиндром
+// console.log('Тест №7 Должно быть true, выводит - ' +
+//   isPolindrom('Лёша на полке клопа нашёл ') // true
+// );
