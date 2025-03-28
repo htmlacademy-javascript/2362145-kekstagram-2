d86353d0e23d809eb6cd673c7a134549f4b79b4a
Nikita7515 <152637405+Nikita7515@users.noreply.github.com>
2025-03-28 16:07:57 +0300

    удалил ненужное

--- js/utils.js
+++ js/utils.js
@@ -25,51 +25,9 @@ const getRandomArrayElement = (elements) => elements[getRandomInteger(0, element
 
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
-const showMessage = (type) => {
-  const template = document.querySelector(`#${type}`).content;
-  const message = template.cloneNode(true);
-  const messageElement = message.querySelector(`.${type}__inner`);
-  const closeButton = message.querySelector(`.${type}__button`);
-
-  document.body.appendChild(message);
-
-  const closeMessage = () => {
-    message.remove();
-    document.removeEventListener('keydown', onEscKeydown);
-    document.removeEventListener('click', onDocumentClick);
-  };
-
-  const onEscKeydown = (evt) => {
-    if (evt.key === 'Escape') {
-      closeMessage();
-    }
-  };
-
-  const onDocumentClick = (evt) => {
-    if (!messageElement.contains(evt.target)) {
-      closeMessage();
-    }
-  };
-
-  closeButton.addEventListener('click', closeMessage);
-  document.addEventListener('keydown', onEscKeydown);
-  document.addEventListener('click', onDocumentClick);
-};
-
-const debounce = (callback, timeoutDelay = 500) => {
-  let timeoutId;
-
-  return (...rest) => {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
-  };
-};
-
 export {
   getRandomInteger,
   createRandomIdFromRangeGenerator,
   getRandomArrayElement,
-  isEscapeKey,
-  showMessage,
-  debounce
+  isEscapeKey
 };
